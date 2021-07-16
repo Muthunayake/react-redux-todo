93a587453dd5941d0deee228940ee62c93f3a3ea
Minoshan Muthunayake <minoshan@fusiongrove.com>
2021-07-16 19:09:17 +0530

    format-codes

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
-import {createStore, applyMiddleware} from 'redux';
-import rootReducer from '../reducers';
-import thunkMiddleware from 'redux-thunk';
+import {createStore, applyMiddleware} from "redux";
+import rootReducer from "../reducers";
+import thunkMiddleware from "redux-thunk";
 
 const store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
 
